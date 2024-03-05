9e9e432b4b149781dffbb1940fb97f9a10c5d06e
Jason Michelson <jasontmichelson@gmail.com>
2024-03-04 20:16:57 -0700

    Update About Us page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import { LampContainer } from '@/components/ui/lamp';
 
 export default function About() {
   return (
-    <div className='mt-20'>
+    <div className='flex-row min-h-screen'>
       <LampContainer>
         <motion.h1
           initial={{ opacity: 0.5, y: 100 }}
@@ -17,9 +17,47 @@ export default function About() {
           }}
           className='mt-8 bg-gradient-to-br from-white to-blue py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl'
         >
-          Our History <br /> from the beginning
+          Our Team
         </motion.h1>
       </LampContainer>
+      <div className='flex flex-row flex-wrap items-start'>
+        <div className='flex flex-col m-10 p-3 max-w-sm bg-slate-700 rounded-lg'>
+          <img className='rounded-lg' src='./vinny_profile.webp' alt="Vinny the brewer"></img>
+          <p className='mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto'>
+            "Yo Bro Brewing is our passion for beer; it's our take at building something together as
+            friends. We've started from scratch, we've gone deep into unknown waters (with yeast, and
+            malt, and hops) to figure out what works and what doesn't work. Anything great takes time
+            and the fermentation process is an art. Running a business isn't easy, there's always more
+            work to be done, but make sure you stick with your bros and talk shit over a beer. Follow
+            the golden rule (use Pilsner malt)!" -&nbsp;Vinny
+          </p>
+        </div>
+        <div className='flex flex-col m-10 p-3 max-w-sm bg-slate-700 rounded-lg'>
+          <img className='rounded-lg' src='./nick_profile.webp' alt="Nick the brewer"></img>
+          <p className='flex mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto'>
+            "Any dedicated brewer will tell you that it takes immense hard work and heart to brew a fine beer at Yo
+            Bro Brewery we agree. In an industry where the product is king, countless hours of research need to be
+            dedicated to even the smallest of processes to ensure all the details are tuned perfectly to make every
+            brew day successful in resulting in the best-tasting beer possible time and time again. Add running a
+            business on top of the already complex nature of brewing and execution becomes even more strenuous
+            and demanding. Here at Yo Bro Brewery, we strongly believe leveraging organization not only as a
+            mantra but as a tool gives us the greatest opportunity at achieving our goal. At
+            the end of the day, our goal here at Yo Bro Brewery is to passionately ensure all our clients have a
+            delicious, refreshing, and well-crafted brew to enjoy amongst friends." -&nbsp;Nick
+          </p>
+        </div>
+        <div className='flex flex-col m-10 p-3 max-w-sm bg-slate-700 rounded-lg'>
+          <img className='rounded-lg' src='./jason_profile.webp' alt="Jason the brewer"></img>
+          <p className='flex mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto'>
+            "Science has been a passion of mine since I was a child. Ever since I can remember I have been striving to
+            learn and improve. Constantly looking for a worthwhile avenue to invest my time in and strive for greatness.
+            Yo Bro is that avenue. Where precision measurements and experimentation are at the forefront of our process.
+            Iterating on something for thousands of hours and nearing perfection, but never quite reaching it.
+            A process I aim to follow for the rest of my life. Creating beer that I can proudly drink.
+            We will never be satisfied and that is our biggest strength as brewers." -&nbsp;Jason
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,8 @@
 const images = [
-  'https://images.unsplash.com/photo-1567696911980-2eed69a46042?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
-  'https://images.unsplash.com/photo-1436076863939-06870fe779c2?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  '/yb_first_beer.webp',
+  '/yb_kegs.webp',
   'https://images.unsplash.com/photo-1600788886242-5c96aabe3757?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1535958636474-b021ee887b13?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-
   'https://images.unsplash.com/photo-1441985969846-3e7c90531139?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1567696911980-2eed69a46042?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1535958636474-b021ee887b13?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
